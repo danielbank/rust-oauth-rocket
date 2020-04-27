73873111be5faf1751ad6e23df6a679c96232cc8
Daniel Bank <daniel.p.bank@gmail.com>
2020-04-26 17:02:22 -0700

    Rocket App with Static Assets

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,13 @@
 #[macro_use]
 extern crate rocket;
 
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
-}
+use rocket_contrib::serve::StaticFiles;
 
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket::ignite()
+        .mount(
+            "/",
+            StaticFiles::from(concat!(env!("CARGO_MANIFEST_DIR"), "/static")),
+        )
+        .launch();
 }

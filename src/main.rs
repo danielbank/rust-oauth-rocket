4ebb857765fccae9ba72c5fc59083cab976d18b8
Daniel Bank <daniel.p.bank@gmail.com>
2020-04-26 17:18:13 -0700

    Rocket App with Static Asset and Another Route

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,17 @@
 #[macro_use]
 extern crate rocket;
 
+use rocket::http::RawStr;
 use rocket_contrib::serve::StaticFiles;
+
+#[get("/callback?<code>&<state>")]
+pub fn auth(code: &RawStr, state: &RawStr) -> String {
+    format!("Code = {}, State = {}", code.to_string(), state.to_string())
+}
 
 fn main() {
     rocket::ignite()
+        .mount("/oauth", routes![auth])
         .mount(
             "/",
             StaticFiles::from(concat!(env!("CARGO_MANIFEST_DIR"), "/static")),

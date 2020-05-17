1b51e9da2e67fee71d56e0550b24c68e4ab29af2
Daniel Bank <daniel.p.bank@gmail.com>
2020-05-17 09:32:51 -0700

    Validate CSRF State using a cookie to share the original with the one returned by Google

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 Example of [OAuth2 Authentication](https://github.com/ramosbugs/oauth2-rs) with the [Rocket Web Framework](https://rocket.rs/). This project was presented at the [Desert Rust](https://rust.azdevs.org/) meetup.
 
+## ⚠️ Experimental Code ⚠️
+
+This is demo code and not meant for production use. The cookie used has `secure` set to `false` and `SameSite::Lax` so that it can be used for testing in localhost. I am also not sure about the use of a cookie for sharing the `csrf_state` between endpoints.
+
 ## Usage
 
 ### Create Google API Credentials
@@ -10,7 +14,7 @@ Example of [OAuth2 Authentication](https://github.com/ramosbugs/oauth2-rs) with 
 
 2. Click **Create Credentials** and choose **OAuth Client ID**
 
-3. Select **Web application** as the Application type, name the credentials something meaningful, and add an Authorized redirect URI. If you are following along with this example, the Authorized redirect URI should be `http://localhost:8080/`. The ending slash in the redirect URI is important.
+3. Select **Web application** as the Application type, name the credentials something meaningful, and add two Authorized redirect URIs. If you are following along with this example, the Authorized redirect URIs should be `http://localhost:8080/` and `http://localhost:8080/oauth/callback`. The ending slash in the first redirect URI is important.
 
 ### Run the Example OAuth Code
 
@@ -38,5 +42,5 @@ rustup override set nightly
 2. Run the Rocket server using the following command:
 
 ```
-cargo run
+GOOGLE_CLIENT_ID=xxx GOOGLE_CLIENT_SECRET=yyy cargo run
 ```
--- src/main.rs
+++ src/main.rs
@@ -8,18 +8,42 @@ use oauth2::reqwest::http_client;
 use oauth2::{
     AuthUrl, AuthorizationCode, ClientId, ClientSecret, CsrfToken, RedirectUrl, Scope, TokenUrl,
 };
-use rocket::http::RawStr;
+use rocket::http::{Cookie, Cookies, RawStr, SameSite};
+use rocket::outcome::IntoOutcome;
+use rocket::request::{self, FromRequest, Request};
 use rocket::response::Redirect;
 use rocket::State;
 use rocket_contrib::serve::StaticFiles;
 use std::env;
 
+#[derive(Debug)]
+struct CsrfState(String);
+
+impl Into<String> for CsrfState {
+    fn into(self) -> String {
+        self.0
+    }
+}
+
+impl<'a, 'r> FromRequest<'a, 'r> for CsrfState {
+    type Error = std::convert::Infallible;
+
+    fn from_request(request: &'a Request<'r>) -> request::Outcome<CsrfState, Self::Error> {
+        request
+            .cookies()
+            .get_private("csrf_state")
+            .and_then(|cookie| cookie.value().parse().ok())
+            .map(|csrf_state| CsrfState(csrf_state))
+            .or_forward(())
+    }
+}
+
 #[get("/authorize")]
-pub fn authorize(client: State<BasicClient>) -> Redirect {
-    // Generate the authorization URL to which we'll redirect the user.
+fn authorize(mut cookies: Cookies<'_>, client: State<BasicClient>) -> Redirect {
+    // Generate the authorization URL to which we'll redirect the user
     let (authorize_url, csrf_state) = client
         .authorize_url(CsrfToken::new_random)
-        // This example is requesting access to the "calendar" features and the user's profile.
+        // This example is requesting access to the "calendar" features and the user's profile
         .add_scope(Scope::new(
             "https://www.googleapis.com/auth/calendar".to_string(),
         ))
@@ -27,25 +51,45 @@ pub fn authorize(client: State<BasicClient>) -> Redirect {
             "https://www.googleapis.com/auth/plus.me".to_string(),
         ))
         .url();
+    // Save the CSRF State Secret in session so we can check it later when callback is called
+    let mut cookie = Cookie::build("csrf_state", csrf_state.secret().clone())
+        .path("/oauth")
+        .secure(false)
+        .http_only(true)
+        .finish();
+    cookie.set_same_site(SameSite::Lax);
+    cookies.add_private(cookie);
     Redirect::to(authorize_url.to_string())
 }
 
+// Endpoint for receiving the callback from the Google OAuth server
 #[get("/callback?<code>&<state>")]
-pub fn callback(client: State<BasicClient>, code: &RawStr, state: &RawStr) -> String {
+fn callback(
+    csrf_state: CsrfState,
+    mut cookies: Cookies<'_>,
+    client: State<BasicClient>,
+    code: &RawStr,
+    state: &RawStr,
+) -> String {
+    // Attempt to decode the code received from Google
     let decoded = code.url_decode();
     match decoded {
         Ok(auth_code) => {
+            // Check that the code from Google matches the code we stored in our session
             let code = AuthorizationCode::new(auth_code);
-            println!("Google returned the following code:\n{}\n", code.secret());
-            let state = CsrfToken::new(state.to_string());
-            // TODO: NEED TO GET THE CSRF_STATE HERE FROM AUTHORIZE()
-            println!(
-                "Google returned the following state:\n{:?} (expected csrf_state.secret() :P)\n",
-                state.secret(),
-                // csrf_state.secret()
+            let returned_state = CsrfToken::new(state.to_string());
+            let session_state: String = csrf_state.into();
+            assert_eq!(
+                returned_state.secret(),
+                &session_state,
+                "Google returned the following state:\n{:?} (expected {:?})\n",
+                returned_state.secret(),
+                &session_state
             );
+            // Remove the code from session, we don't need it anymore
+            cookies.remove_private(Cookie::named("csrf_state"));
 
-            // TODO: SEND THE TOKEN REQUEST USING ROCKET OR AT LEAST HYPER
+            // Exchange the code for a token
             let token = client.exchange_code(code).request(http_client);
             println!("Google returned the following token:\n{:?}\n", token);
             format!("Go back to your terminal :)")
@@ -55,6 +99,7 @@ pub fn callback(client: State<BasicClient>, code: &RawStr, state: &RawStr) -> St
 }
 
 fn main() {
+    // Get ClientId and ClientSecret from Environment Variables and define Google endpoints
     let google_client_id = ClientId::new(
         env::var("GOOGLE_CLIENT_ID").expect("Missing the GOOGLE_CLIENT_ID environment variable."),
     );
@@ -62,19 +107,20 @@ fn main() {
         env::var("GOOGLE_CLIENT_SECRET")
             .expect("Missing the GOOGLE_CLIENT_SECRET environment variable."),
     );
-
     let auth_url = AuthUrl::new("https://accounts.google.com/o/oauth2/v2/auth".to_string());
     let token_url = TokenUrl::new("https://www.googleapis.com/oauth2/v3/token".to_string());
+    let redirect_url = RedirectUrl::new("http://localhost:8080/oauth/callback".to_string());
+
+    // Set up the config for the Google OAuth2 process
     let client = BasicClient::new(
         google_client_id,
         Some(google_client_secret),
         auth_url.unwrap(),
         Some(token_url.unwrap()),
     )
-    .set_redirect_url(
-        RedirectUrl::new("http://localhost:8080/oauth/callback".to_string()).unwrap(),
-    );
+    .set_redirect_url(redirect_url.unwrap());
 
+    // Launch the Rocket web server
     rocket::ignite()
         .manage(client)
         .mount("/oauth", routes![authorize, callback])
